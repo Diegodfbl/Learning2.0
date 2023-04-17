7a2d1718e40c01e7e629674679d9d92875f18566
Diego Fernando B Lima <126214378+Diegodfbl@users.noreply.github.com>
2023-04-16 21:54:00 -0300

    Add files via upload

---
+++ Backend/DotNet/Aula 1/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+// INSTRUÇÃO DE NÍVEL SUPERIOR. CRU e nada mais. somente 1 arq por projeto assim.
+using System.Diagnostics;
+using FluentColorConsole;
+
+[DebuggerDisplay($"{{{nameof(GetDebuggerDisplay)}(),nq}}")]
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Digite seu nome:");
+        string nome = Console.ReadLine();
+        Console.WriteLine($"Você digitou: {nome}");
+
+        var textLine = ColorConsole.WithBlueText;
+        textLine.WriteLine("Tchau!");
+    }
+
+    private string GetDebuggerDisplay()
+    {
+        return ToString();
+    }
+}
