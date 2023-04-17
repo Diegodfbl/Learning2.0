85d81c02f8dc05b03ebe262323fae6844a651ab3
Diego Fernando B Lima <126214378+Diegodfbl@users.noreply.github.com>
2023-04-16 21:58:56 -0300

    Add files via upload

---
+++ Backend/C#/Mod 3 - Estrutura de controle/Switch-case_GoTo/Switch-case_GoTo/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Switch_case_GoTo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        #region Switch_Case
+        //SWITCH CASE - Escolhe uma seção com base em uma correspondência
+
+        //Console.WriteLine("Programa de Conversão, nº em Dia da Semana");
+        //Console.WriteLine("Digite um numero entre 1 a 7");
+        //int diaSemana = int.Parse(Console.ReadLine());
+
+
+        //switch (diaSemana)
+        //{
+        //    default:
+        //        Console.WriteLine("Número digitado é inválido. Favor refazer.");
+        //        break;
+
+        //    case 1: // como o switch testa casos da variável teste, que é inteiro, só podemos colocar o caso como numero inteiro
+        //        Console.WriteLine("DOMINGO");
+        //        break; // todo caso precisa ter um break pois o switch executa o caso até achar um break
+
+        //    case 2:
+        //        Console.WriteLine("Segunda-feira");
+        //        break;
+
+        //    case 3:
+        //        Console.WriteLine("Terça-feira");
+        //        break;
+
+        //    case 4:
+        //        Console.WriteLine("Quarta-feira");
+        //        break;
+        //    case 5:
+        //        Console.WriteLine("Quinta-feira");
+        //        break;
+        //    case 6:
+        //        Console.WriteLine("Sexta-feira");
+        //        break;
+        //    case 7:
+        //        Console.WriteLine("Sábado");
+        //        break;
+        //}
+        #endregion
+
+        Inicio: // LABEL = INICIO< retomada caso o usuário digite algo fora do esperado.
+            //Um LABEL pode ser personalizado de acordo com o nome que eu preferir, pode estar em qqr lugar do código
+
+            Console.Write("Teste de Opções, escolha entre Opção, 1 ou 2: ");
+            int op = int.Parse(Console.ReadLine());
+            int valor = 0;
+
+            switch (op)
+            {
+                default:
+                    Console.WriteLine("Opção inválida, tente novamente.");
+                    Console.WriteLine("");
+                    goto Inicio; // Se o usuáio digitar algo fora das opções ele tem a opção de reiniciar o teste, pois ele volta la pro LABEL Inicio.
+                    
+                    case 1:
+                    valor += 100;
+                    break;
+                    case 2:
+                    valor += 50;
+                    goto case 1;
+
+            }
+            Console.WriteLine("O Valor é:" + valor);
+            Console.WriteLine();
+
+            Console.WriteLine("Pressione qualquer tecla para sair...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Backend/C#/Mod 4 - Estrutura de Repetição/Estrutura-Repeticao/Estrutura-Repeticao/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estrutura_Repeticao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region WHILE
+            // WHILE - Repete a estrutura enquanto a operação retornar TRUE, quando for FALSE ele sai da estrutura.
+            //int valor = 0;
+
+            //while (valor < 10)
+            //{
+            //    Console.WriteLine($"O Valor é {valor}");
+            //    valor++;
+            //}
+            #endregion
+
+            #region DO-WHILE
+            //Primeiro ele executa a estrutura de repetição para depois fazer o teste da condição, se TRUE ele continua.
+            //int valor = 0;
+            //do 
+            //{
+            //    Console.WriteLine($"O valor é: {valor} ");
+            //    valor++;
+            //} while (valor < 10);
+
+            #endregion
+
+            #region FOR
+
+            //for (int i = 1, j = 10; i <= 10; i++, j--) // 3 seções separadas por ; 1- variavel de controle(executada somente na 1ª passagem). 2- condição(verificação). 3- incremento.
+            //{
+            //    Console.Write($" O valor de I é: {i}| ");
+            //    Console.WriteLine($"| E de J é: {j}");
+            //}
+
+            #endregion
+
+            #region FOREACH
+
+            //string[] nomes = { "Diego", "Fernando", "Bortholace", "Lima" };
+            //string nome;
+
+            //foreach (string name in nomes) // var item in Collection "var" para quando nao se sabe o tipo de arquivos que vai tirar do array || 
+            //{
+            //    Console.WriteLine($"O nome é: {name}");
+            //}
+
+            #endregion
+
+            Console.WriteLine();
+            Console.WriteLine("Pressione qualquer tecla para sair...");
+            Console.ReadKey();
+        }
+    }
+}
